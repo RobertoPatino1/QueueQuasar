b2622d73214f3781dfb73a92a97b598e4e4cd131
RobertoPatino1 <rapatino@espol.edu.ec>
2023-08-20 18:39:30 -0500

    Added more modularity to the code

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 CC = gcc
 CFLAGS = -Wall -Wextra
 
-all: productor
+productor: productor.o productor_utils.o
+	$(CC) $(CFLAGS) -o productor productor.o productor_utils.o
 
-productor: productor.c
-	$(CC) $(CFLAGS) -o $@ $^
+productor.o: productor.c productor_utils.h
+	$(CC) $(CFLAGS) -c productor.c
+
+productor_utils.o: productor_utils.c productor_utils.h
+	$(CC) $(CFLAGS) -c productor_utils.c
 
 clean:
-	rm -f productor
-
-.PHONY: clean
+	rm -f productor *.o
--- productor.c
+++ productor.c
@@ -2,80 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "productor_utils.h"
 
-typedef struct
-{
-    char *idParticion;
-} TopicState;
 char *nombreTopico1 = "memoria";
 char *nombreTopico2 = "cpu";
 
-void getMemoryUsage(char **memoryUsage)
-{
-    char buffer[128];
-    FILE *fp = popen("free -h | awk 'NR==2{print $3}'", "r");
-    if (fp == NULL)
-    {
-        perror("Error executing free");
-        return;
-    }
-
-    if (fgets(buffer, sizeof(buffer), fp) != NULL)
-    {
-        *memoryUsage = strdup(buffer);
-    }
-
-    pclose(fp);
-}
-
-void getCPUUsage(char **cpuUsage)
-{
-    char buffer[128];
-    FILE *fp = popen("mpstat 1 1 | awk '/all/ {print 100 - $NF\"%\"}'", "r");
-    if (fp == NULL)
-    {
-        perror("Error executing mpstat");
-        return;
-    }
-
-    if (fgets(buffer, sizeof(buffer), fp) != NULL)
-    {
-        *cpuUsage = strdup(buffer);
-    }
-
-    pclose(fp);
-}
-
-char *buildMessage(char *nombreNodo, char *nombreTopico, char *valorTopico, TopicState *topicState, int useRoundRobin)
-{
-    valorTopico[strcspn(valorTopico, "\r\n")] = '\0';
-
-    if (useRoundRobin)
-    {
-
-        if (strcmp(topicState->idParticion, "1") == 0)
-        {
-            topicState->idParticion = "2";
-        }
-        else
-        {
-            topicState->idParticion = "1";
-        }
-    }
-
-    size_t totalSize = strlen(nombreNodo) + strlen(nombreTopico) + strlen(valorTopico) + strlen(topicState->idParticion) + 4;
-
-    char *mensaje = (char *)malloc(totalSize);
-    if (mensaje == NULL)
-    {
-        perror("Error allocating memory");
-        return NULL;
-    }
-
-    snprintf(mensaje, totalSize, "%s/%s|%s|%s", nombreNodo, nombreTopico, valorTopico, topicState->idParticion);
-
-    return mensaje;
-}
 int main(int argc, char *argv[]) //./productor <nombre_nodo> <puerto_productor> <ip_broker> <puerto_broker> [id_particion_topico_1] [id_particion_topico_2]
 {
     if (argc < 5)
