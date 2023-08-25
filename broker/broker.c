78653ca8d1ddb571ac5f9c74333ac6c38bdc8096
RobertoPatino1 <rapatino@espol.edu.ec>
2023-08-25 18:23:39 -0500

    Changed variable names

--- broker/broker.c
+++ broker/broker.c
@@ -9,13 +9,13 @@
 #define MAX_MESSAGE_LENGTH 256
 
 pthread_mutex_t mutex;
-void *handle_gateway(void *arg)
+void *handle_productor(void *arg)
 {
-    int gateway_sock = *(int *)arg;
+    int productor_sock = *(int *)arg;
     char message[MAX_MESSAGE_LENGTH];
     int read_size;
 
-    while ((read_size = recv(gateway_sock, message, sizeof(message), 0)) > 0)
+    while ((read_size = recv(productor_sock, message, sizeof(message), 0)) > 0)
     {
         message[read_size] = '\0';
 
@@ -24,43 +24,40 @@ void *handle_gateway(void *arg)
         pthread_mutex_unlock(&mutex);
     }
 
-    // Si el hilo sale del bucle, es porque la conexión con el gateway se ha cerrado
     printf("El productor se ha desconectado\n");
-    close(gateway_sock);
+    close(productor_sock);
     free(arg);
     pthread_exit(NULL);
 }
 
-// Función para manejar las conexiones del gateway
-void *handle_gateway_connections(void *arg)
+void *handle_productor_connections(void *arg)
 {
-    int broker_sock_gateway = *(int *)arg;
-    struct sockaddr_in broker_addr_gateway;
+    int broker_sock_productor = *(int *)arg;
+    struct sockaddr_in broker_addr_productor;
     int c = sizeof(struct sockaddr_in);
 
     while (1)
     {
-        int gateway_sock = accept(broker_sock_gateway, (struct sockaddr *)&broker_addr_gateway, (socklen_t *)&c);
-        if (gateway_sock < 0)
+        int productor_sock = accept(broker_sock_productor, (struct sockaddr *)&broker_addr_productor, (socklen_t *)&c);
+        if (productor_sock < 0)
         {
-            printf("Error al aceptar la conexión del gateway\n");
+            printf("Error accepting productor connection\n");
             continue;
         }
 
         // Mostrar información de la conexión del Productor
-        printf("Productor conectado desde %s:%d\n", inet_ntoa(broker_addr_gateway.sin_addr), ntohs(broker_addr_gateway.sin_port));
+        printf("Productor connected from %s:%d\n", inet_ntoa(broker_addr_productor.sin_addr), ntohs(broker_addr_productor.sin_port));
 
         // Crear hilo para manejar al productor
-        pthread_t gateway_thread_id;
-        int *gateway_sock_ptr = (int *)malloc(sizeof(int));
-        *gateway_sock_ptr = gateway_sock;
-        if (pthread_create(&gateway_thread_id, NULL, handle_gateway, (void *)gateway_sock_ptr) != 0)
+        pthread_t productor_thread_id;
+        int *productor_sock_ptr = (int *)malloc(sizeof(int));
+        *productor_sock_ptr = productor_sock;
+        if (pthread_create(&productor_thread_id, NULL, handle_productor, (void *)productor_sock_ptr) != 0)
         {
-            printf("Error al crear el hilo para el Gateway\n");
+            printf("Error creating productor thread\n");
             return NULL;
         }
-        // Liberar el hilo principal para aceptar más conexiones del gateway
-        pthread_detach(gateway_thread_id);
+        pthread_detach(productor_thread_id);
     }
 
     return NULL;
@@ -70,67 +67,62 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        printf("Uso: %s <puerto_gateway> <puerto_subscribers>\n", argv[0]);
+        printf("Uso: %s <puerto_productor> <puerto_consumidor>\n", argv[0]);
         return 1;
     }
 
-    int broker_port_gateway = atoi(argv[1]);
+    int broker_port_productor = atoi(argv[1]);
     int broker_port_subscribers = atoi(argv[2]);
-    if (broker_port_gateway <= 0 || broker_port_subscribers <= 0)
+    if (broker_port_productor <= 0 || broker_port_subscribers <= 0)
     {
         printf("Error: Puertos inválidos\n");
         return 1;
     }
 
-    int broker_sock_gateway, broker_sock_subscribers;
-    struct sockaddr_in broker_addr_gateway, broker_addr_subscribers;
+    int broker_sock_productor, broker_sock_subscribers;
+    struct sockaddr_in broker_addr_productor, broker_addr_subscribers;
 
-    // Configurar socket para el gateway
-    broker_sock_gateway = socket(AF_INET, SOCK_STREAM, 0);
-    if (broker_sock_gateway == -1)
+    broker_sock_productor = socket(AF_INET, SOCK_STREAM, 0);
+    if (broker_sock_productor == -1)
     {
-        printf("No se pudo crear el socket para el gateway\n");
+        printf("Error: Unable to create socket\n");
         return 1;
     }
 
-    broker_addr_gateway.sin_family = AF_INET;
-    broker_addr_gateway.sin_addr.s_addr = INADDR_ANY;
-    broker_addr_gateway.sin_port = htons(broker_port_gateway);
+    broker_addr_productor.sin_family = AF_INET;
+    broker_addr_productor.sin_addr.s_addr = INADDR_ANY;
+    broker_addr_productor.sin_port = htons(broker_port_productor);
 
-    if (bind(broker_sock_gateway, (struct sockaddr *)&broker_addr_gateway, sizeof(broker_addr_gateway)) < 0)
+    if (bind(broker_sock_productor, (struct sockaddr *)&broker_addr_productor, sizeof(broker_addr_productor)) < 0)
     {
-        printf("Error en la operación de bind para el gateway, asegúrese de utilizar un puerto válido\n");
+        printf("Error: Try to use a valid port\n");
         return 1;
     }
 
-    listen(broker_sock_gateway, 10);
-    printf("Broker inicializado para el gateway en el puerto %d\n", broker_port_gateway);
+    listen(broker_sock_productor, 10);
+    printf("Successfully initialized broker using the port: %d\n", broker_port_productor);
 
-    // Inicializar el mutex
     if (pthread_mutex_init(&mutex, NULL) != 0)
     {
         printf("Error al inicializar el mutex\n");
         return 1;
     }
 
-    // Crear hilo para manejar las conexiones del gateway
-    pthread_t gateway_connections_thread_id;
-    if (pthread_create(&gateway_connections_thread_id, NULL, handle_gateway_connections, (void *)&broker_sock_gateway) != 0)
+    pthread_t productor_connections_thread_id;
+    if (pthread_create(&productor_connections_thread_id, NULL, handle_productor_connections, (void *)&broker_sock_productor) != 0)
     {
-        printf("Error al crear el hilo para manejar las conexiones del gateway\n");
+        printf("Error creating connections thread\n");
         return 1;
     }
 
-    // Mantener el hilo principal en ejecución
     while (1)
     {
         sleep(1);
     }
 
-    // Destruir el mutex antes de salir
     pthread_mutex_destroy(&mutex);
 
-    close(broker_sock_gateway);
+    close(broker_sock_productor);
     close(broker_sock_subscribers);
     return 0;
 }
